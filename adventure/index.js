bcf1ebf0f8de08b2389f552278d625df03cb3324
ADRIAN <adriangallery@adriancerda.com>
2025-07-29 15:48:26 +0200

    Cambiar duración de mensajes popup de 2 a 4 segundos

--- adventure/index.js
+++ adventure/index.js
@@ -1485,7 +1485,7 @@ function showFloatingText(message, x, y) {
         if (floatingText.parentNode) {
             floatingText.remove();
         }
-    }, 2000);
+    }, 4000);
 }
 
 // Handle mouse movement for cursor feedback
